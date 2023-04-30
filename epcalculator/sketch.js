529c34f6ebde35bcbd1e744db634c7ba14686ac9
MossCarpet <rayyanvalliani@gmail.com>
2023-04-29 22:39:23 -0600

    Finished main calculator idea
    Completed the calculator, I was gonna add a answer history but since I made the background update over and over again which is required for the whole thing to work I was un able to add it.

--- epcalculator/sketch.js
+++ epcalculator/sketch.js
@@ -9,6 +9,7 @@ Notes:
 let Mass = 0 
 let Gravity = 9.81
 let Height = 0
+let rows = 600;
 
 let massButtonColor = "black"
 
@@ -57,6 +58,11 @@ function draw(){
   textSize("14");
   text("Set Mass", 140,310)
 
+  fill("black")
+  textFont(kanit)
+  textSize("12")
+  text("Mass: " + Mass, 150,390)
+
   // Uses rectMode2 to change the text and background colour of the button when cursor is hovering on button. Gives more of a buttony look
   if (overRect2(200, 300, 200,100)){
     
@@ -76,6 +82,11 @@ function draw(){
   textSize("14");
   text("Set Height", 460, 310);
 
+  fill("black")
+  textFont(kanit)
+  textSize("12")
+  text("Height: " + Height, 470,390)
+
   btnclr = color("black");
 
   // If the height or mass are larger than 0 then it will calculate ep
@@ -84,8 +95,8 @@ function draw(){
     fill("black");
     textFont("Helvetica");
     textSize(25);
-    text("Your answer is: " + ep + " J", 260, 450);
-    console.log(ep + " J");
+    text("Your answer is: " + ep + " J", 260, 450)
+    console.log(ep + " J" + " Is your answer");
   } else if(Height<0||Mass<0) { // If mass and or height is inputted as negative it will show an error
     fill("black");
     textFont("Helvetica");
@@ -99,6 +110,11 @@ function draw(){
     text("Please input Mass and Height", 213, 450);
   }
 
+
+  
+
+
+  // Shows keybinds that the user can use to automatically increase/decrease mass or height
   fill("black");
   textFont("Helvetica");
   textSize(30);
@@ -106,11 +122,19 @@ function draw(){
   fill("black")
   textFont("Helvetica");
   textSize(23)
-  text("J = Increase Mass", 276, 605)
+  text("J = Increase Mass", 272, 605)
   fill("black")
   textFont("Helvetica");
   textSize(23)
-  text("K = Increase Height", 272, 645)
+  text("K = Increase Height", 270, 645)
+  fill("black")
+  textFont("Helvetica");
+  textSize(23)
+  text("N = Decrease Mass", 270, 685)
+  fill("black")
+  textFont("Helvetica");
+  textSize(23)
+  text("M = Decrease Height", 268, 725)
 
   // Changes the cursor if hovering on either buttons (|| = "or")
   if(mouseX>=100&&mouseX<=300&&mouseY>=250&&mouseY<=350||mouseX>=430&&mouseX<=630&&mouseY>=250&&mouseY<=350) {
@@ -118,9 +142,8 @@ function draw(){
   } else {
     cursor(ARROW)
   }
-  
-}
 
+}
 
 // Checks if mouse if over the buttons and if so it returns true as a value.
 function overRect(x, y, w, h){
@@ -145,31 +168,34 @@ function mousePressed(){
   if(mouseX>=100&&mouseX<=300&&mouseY>=250&&mouseY<=350){
     Mass = 0
     Mass = window.prompt("What is your objects mass?")
-    console.log(Mass)
   }
 
   // When height button is pressed the script will prompt the user to input mass
   if(mouseX>=430&&mouseX<=630&&mouseY>=250&&mouseY<=350){
     Height = 0
     Height = window.prompt("What is your objects height?")
-    console.log(Height)
   }
 }
 
+// When J, K, N,M are pressed this function is ran and mass and or height are changed. The "rows" variable is increased by 40
 function keyPressed() {
   if(keyCode==74){
+    rows = rows + 40
     Mass = Mass - -1
   }
 
   if(keyCode==75){
+    rows = rows + 40
     Height = Height- -1
   }
 
   if(keyCode==78){
+    rows = rows + 40
     Mass = Mass - 1
   }
 
   if(keyCode==77){
+    rows = rows + 40
     Height = Height - 1
   }
 }
